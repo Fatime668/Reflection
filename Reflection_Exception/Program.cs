95f721bf909c2f216ad16d9a2f512ea2140c20f9
Fatime668 <tu7dkjdue@code.edu.az>
2022-03-25 16:08:53 +0400

    Reflection

---
+++ Reflection_Exception/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task-1
+            Console.WriteLine(isOdd(15));
+            Console.WriteLine(isEven(15));
+
+        }
+        #region task-1
+        public static bool isOdd(int num)
+        {
+            if (num % 2 == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool isEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
